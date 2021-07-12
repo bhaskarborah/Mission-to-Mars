b913f250b0e7589f7324c20d7ced5647f9ccbf2a
bhaskarborah <bhaskar.borah1984@gmail.com>
2021-07-12 16:15:00 -0700

    Add files via upload
    Updating the new code

---
+++ Mission_to_Mars_Challenge.py
@@ -0,0 +1,111 @@
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('div.list_text')
+
+slide_elem.find('div', class_='content_title')
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+# ### Featured Images
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['description', 'Mars', 'Earth']
+df.set_index('description', inplace=True)
+df
+
+df.to_html()
+
+browser.quit()
+
+
+# ### D1: Scrape High-Resolution Mars’ Hemisphere Images and Title
+
+# 1. Use browser to visit the URL 
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+browser.visit(url)
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+# Parse the html with soup
+html = browser.html
+main_page_soup = soup(html, 'html.parser')
+
+# Find the number of pictures to scan
+total_pics = len(main_page_soup.select("div.item"))
+
+# for loop over the link of each sample picture
+for i in range(total_pics):
+    # Create an empty dict to hold the search results
+    results = {}
+    # Find link to picture and open it
+    link_image = main_page_soup.select("div.description a")[i].get('href')
+    browser.visit(f'https://astrogeology.usgs.gov{link_image}')
+    
+    # Parse the new html page with soup
+    html = browser.html
+    sample_image_soup = soup(html, 'html.parser')
+    # Get the full image link. This search gives the correct image name
+    img_url = sample_image_soup.select_one("div.downloads ul li a").get('href')
+    # Get the full image title
+    img_title = sample_image_soup.select_one("h2.title").get_text()
+    # Add extracts to the results dict
+    results = {
+        'img_url': img_url,
+        'title': img_title}
+    
+    # Append results dict to hemisphere image urls list
+    hemisphere_image_urls.append(results)
+    print(hemisphere_image_urls)
+    
+    # Return to main page
+    browser.back()
+
+# 4. Print the list that holds the dictionary of each image url and title.
+hemisphere_image_urls
+
+# 5. Quit the browser
+browser.quit()
+
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Mission-to-Mars
+#   Mission-To-Mars-Analysis
+
+## Overview of Project
+
+Robin is a Junior Data Scientist who does freelance astronomy work in spare time. Robin dreams to land a job in NASA and keeps on visiting astronomy web sites to collect information. She is specially interested in Mars and frequently visits websites related to Mission to Mars.
+While visiting one of the sites, she has a question as to how much water did the Red Planet have in its youth.
+She wants to gather all the required information in one location from various web sites, and then show it off to other astrophiles.
+The objective of this project is to:
+Write a python script to scrape through data from different websites.
+Store the collected data into a NoSQL Database - Mongo DB
+Put the collected information into a Web Application using Flask
+
+## Purpose
+
+The required information is available in multiple websites.
+
+The purpose of this project is to provide the below deliverables:
+
+Deliverable 1: Scrape Full-Resolution Mars Hemisphere Images and Titles
+Deliverable 2: Update the Web App with Mars Hemisphere Images and Titles
+Deliverable 3: Add Bootstrap 3 Components and display the results
+
+We have to scrape Mars data such as the latest images, headlines, table data and hemisphere images, then store this information into MongoDB and display the required information in a web-application using Flask, HTML and Bootstrap.
+
+## Results
+
+Deliverable 1: Scrape Full-Resolution Mars Hemisphere Images and Titles
+
+The Hemisphere Images and Titles have been scraped using the below code:
+![Screen Shot 2021-07-12 at 4.03.58 PM](https://i.imgur.com/KKMBE2j.png)
+
+This code provides us with the Hemisphere Images and Titles, which we can use to display on our web application
+
+![Screen Shot 2021-07-12 at 4.05.28 PM](https://i.imgur.com/E2Q0f7z.png)
+
+Deliverable 2: Update the Web App with Mars Hemisphere Images and Titles
+
+The Mars Hemisphere Images and Titles are displayed as below:
+
+![Screen Shot 2021-07-12 at 4.07.17 PM](https://i.imgur.com/Y775Z1I.png)
+
+These details are displayed by using the below HTML code:
+![Screen Shot 2021-07-12 at 4.08.15 PM](https://i.imgur.com/MjmzR4H.png)
+
+Deliverable 3: Add Bootstrap 3 Components and display the results
+
+The bootstrap details have been added to display the results:
+![Screen Shot 2021-07-12 at 4.08.15 PM](https://i.imgur.com/B7pj8fg.png)
+
+The complete web application is as below:
+![Screen Shot 2021-07-12 at 4.10.41 PM](https://i.imgur.com/X8kL9N9.png)
+
+# Mission-To-Mars-Summary
+
+The required details have been displayed into a web application using Python, MongoDb, Flask and Bootstrap/HTML.
+
+The "Scrape New Data" button needs to be clicked and updated information would be scraped and displayed in the web application
+
+![Screen Shot 2021-07-12 at 4.10.41 PM](https://i.imgur.com/QBC8xnq.png)
